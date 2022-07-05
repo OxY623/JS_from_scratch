5a38d895dc9c6e609937c3b19886b8533dd08f82
OxY623 <fuzzydoorproductions0@gmail.com>
2022-07-05 12:24:03 +0300

    update

--- 2/script.js
+++ 2/script.js
@@ -1,1 +1,15 @@
-alert('Hello, OxY!');
+window.onload = function () {
+  function fShow() {
+    let divTime = document.getElementById("time");
+    divTime.style.visibility = "visible";
+  }
+
+  function fTick() {
+    let date = new Date();
+    let t = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    let divTime = document.getElementById("time");
+    divTime.innerHTML = t;
+  }
+  setTimeout(fShow, 2000);
+  setInterval(fTick, 1000);
+};
