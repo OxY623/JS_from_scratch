e400cb74926f003ce18cd1d01700847b9004c8f9
OxY623 <fuzzydoorproductions0@gmail.com>
2022-08-10 14:25:38 +0300

    Summary

--- 3_snake/game.js
+++ 3_snake/game.js
@@ -1,96 +1,97 @@
 var KEYS = {
-  LEFT: 37,
-  UP: 38,
-  RIGHT: 39,
-  DOWN: 40
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40
 }
 
 var DEFAULT_COURSE = 'up';
 
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function Game(matrix, snake) {
 
- let  _this = this;
+    var _this = this;
 
-  document.onkeydown = function(e) {
-      e = e || window.event
+    document.onkeydown = function(e) {
+        e = e || window.event
 
-      switch(e.keyCode)
-      {
-          case KEYS.LEFT:
-              _this.snake.course = 'left';
-              break;
-          case KEYS.UP:
-              _this.snake.course = 'up';
-              break;
-          case KEYS.RIGHT:
-              _this.snake.course = 'right';
-              break;
-          case KEYS.DOWN:
-              _this.snake.course = 'down';
-              break;
+        switch(e.keyCode)
+        {
+            case KEYS.LEFT:
+                _this.snake.course = 'left';
+                break;
+            case KEYS.UP:
+                _this.snake.course = 'up';
+                break;
+            case KEYS.RIGHT:
+                _this.snake.course = 'right';
+                break;
+            case KEYS.DOWN:
+                _this.snake.course = 'down';
+                break;
 
-      }
-  }
+        }
+    }
 
-  this.makeSnake = function() {
-      if (_this.snake) {
-          for(var i =0; i < _this.snake.body.length; i++) {
-              _this.matrix.setCell(_this.snake.body[i].row, _this.snake.body[i].col, false);
-          }  
-      }
-      _this.snake = new Snake(5, 5, DEFAULT_COURSE, 3);
-      for(var i =0; i < _this.snake.body.length; i++) {
-          _this.matrix.setCell(_this.snake.body[i].row, _this.snake.body[i].col, true);
-      }
-  }
+    this.makeSnake = function() {
+        if (_this.snake) {
+            for(var i =0; i < _this.snake.body.length; i++) {
+                _this.matrix.setCell(_this.snake.body[i].row, _this.snake.body[i].col, false);
+            }  
+        }
+        _this.snake = new Snake(5, 5, DEFAULT_COURSE, 3);
+        for(var i =0; i < _this.snake.body.length; i++) {
+            _this.matrix.setCell(_this.snake.body[i].row, _this.snake.body[i].col, true);
+        }
+    }
 
-  this.resetGame = function() {
-      alert("you lost");
-      _this.makeSnake();         
-  }
+    this.resetGame = function() {
+        alert("you lost");
+        _this.makeSnake();         
+    }
 
-  this.gameplay = function() {
-     let  tail = _this.snake.move();
-     let  head = _this.snake.body[0];
+    this.gameplay = function() {
+        var tail = _this.snake.move();
+        var head = _this.snake.body[0];
 
-      if ((head.row > _this.matrix.rows || head.row < 1 ||
-          head.col > _this.matrix.cols || head.col < 1) ||
-          _this.matrix.getCell(head.row, head.col)) {
-          _this.resetGame();
-          _this.matrix.setCell(tail.row, tail.col, false);
-          return;
-      }
+        if ((head.row > _this.matrix.rows || head.row < 1 ||
+            head.col > _this.matrix.cols || head.col < 1) ||
+            _this.matrix.getCell(head.row, head.col)) {
+            _this.resetGame();
+            _this.matrix.setCell(tail.row, tail.col, false);
+            return;
+        }
 
-      if(_this.matrix.getFruit(head.row, head.col)) {
-          _this.snake.grow();
-          _this.matrix.setFruit(head.row, head.col, false);
-          _this.setRandomFruit();
-      }
+        if(_this.matrix.getFruit(head.row, head.col)) {
+            _this.snake.grow();
+            _this.matrix.setFruit(head.row, head.col, false);
+            _this.setRandomFruit();
+        }
 
-      _this.matrix.setCell(head.row, head.col, true);
-      _this.matrix.setCell(tail.row, tail.col, false);
-  }
-
-
-
-  this.setRandomFruit = function() {
-     let  fruitRow = getRandomInt(1, _this.matrix.rows);
-     let  fruitCol = getRandomInt(1, _this.matrix.cols);
-      _this.matrix.setFruit(fruitRow, fruitCol, true);
-  }
+        _this.matrix.setCell(head.row, head.col, true);
+        _this.matrix.setCell(tail.row, tail.col, false);
+    }
 
 
-  this.matrix = new Matrix('#matrix1', 20, 20);
-  this.matrix.create();
-  this.makeSnake();
-  this.setRandomFruit();
+
+    this.setRandomFruit = function() {
+        var fruitRow = getRandomInt(1, _this.matrix.rows);
+        var fruitCol = getRandomInt(1, _this.matrix.cols);
+        _this.matrix.setFruit(fruitRow, fruitCol, true);
+    }
+
+
+    this.matrix = new Matrix('#matrix1', 20, 20);
+    this.matrix.create();
+    this.makeSnake();
+    this.setRandomFruit();
 }
 
 $(document).ready(function() {
- let  game = new Game();
-  setInterval(game.gameplay, 250);
+    var game = new Game();
+    setInterval(game.gameplay, 250);
 });
+
--- 3_snake/snake.js
+++ 3_snake/snake.js
@@ -1,38 +1,38 @@
-function Snake(rol,col,course, lenght) {
-    let _this=this;
-    this.body = [];
-    for(var i=0; i < lenght; i++) {
-		_this.body.push({row: row + i, col: col});
+function Snake(row, col, course, lenght) {
+	this.body = [];
+
+	for(var i=0; i < lenght; i++) {
+		this.body.push({row: row + i, col: col});
 	}
-    this.course=course;
-   
+	this.course = course;
+	var _this = this; // то = это
 
-    this.grow = function(){
-        let tail = _this.body[_this.body.length-1];
-        _this.body.push(tail);
-    }
+	this.grow = function() {
+		var tail = _this.body[_this.body.length - 1];
+		_this.body.push(tail);
+	}
 
-    this.move = function(){
-        let headRow = _this.body[0].row;
-        let headCol = _this.body[0].col;
+	this.move = function () {
+		var headRow = _this.body[0].row;
+		var headCol = _this.body[0].col;
 
-        switch (_this.course){
-            case 'right':
-                headCol++;
-                break;
-            case 'left':
-                headCol--;
-                break;
-            case 'up':
-                headRow--;
-                break;
-            case 'down':
-                headRow++;
-                break;
-        }
-        _this.body.shift({row:headRow,col:headCol});
-        return _this.body.pop();
-    }
+		switch (_this.course) {
+			case 'right':
+				headCol++;
+				break;
+			case 'left':
+				headCol--;
+				break;
+			case 'up':
+				headRow--;
+				break;
+			case 'down':
+				headRow++;
+				break;
+		}
 
+		_this.body.unshift({row: headRow, col: headCol});
 
+		return _this.body.pop();
+	}
 }
